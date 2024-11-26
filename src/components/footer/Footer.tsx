ff514d30b2a5ba073926214981dd504fbd95fa42
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-11-27 02:10:33 +0500

    Added images, <Navbar /> and <Footer />

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,5 +1,10 @@
 import "./footer.scss";
 
 export default function Footer() {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <span>PrimePanel</span>
+      <span>&copy; PrimePanel Admin Dashboard</span>
+    </div>
+  );
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,4 +1,25 @@
 import "./navbar.scss";
 export default function Navbar() {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <img src="logo.png" alt="Logo" />
+        <span>PrimePanel</span>
+      </div>
+      <div className="icons">
+        <img src="/search.svg" alt="search-logo" />
+        <img src="/app.svg" alt="app-logo" />
+        <img src="/expand.svg" alt="expand-logo" />
+        <div className="notification">
+          <img src="/notifications.svg" alt="notifications-logo" />
+          <span>1</span>
+        </div>
+        <div className="user">
+          <img src="/Ahmad.jpeg" alt="profile" />
+          <span>Ahmad</span>
+        </div>
+        <img src="/settings.svg" alt="settings-logo" />
+      </div>
+    </div>
+  );
 }
